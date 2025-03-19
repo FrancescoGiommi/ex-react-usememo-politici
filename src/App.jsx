9564869cb9d9bc932d64b876e8e7951ebc7a0291
Francesco <you@example.com>
2025-03-19 10:58:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import "./App.css";
+
+//! Milestone 1: Recuperare e visualizzare i dati
+
+/* Effettua una chiamata API a
+    https://boolean-spec-frontend.vercel.app/freetestapi/politicians
+
+    Salva la risposta in uno stato React (useState).
+
+    Mostra i politici in una lista di card, visualizzando almeno le seguenti proprietà:
+        Nome (name)
+        Immagine (image)
+        Posizione (position)
+        Breve biografia (biography)
+
+Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile. */
+
+function App() {
+  const [politicians, setPoliticians] = useState([]);
+
+  return <></>;
+}
+
+export default App;
