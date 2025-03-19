5b78f2bad20f85a8d7b65e75be181245706811af
Francesco <you@example.com>
2025-03-19 16:28:11 +0100

    Add milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 
@@ -19,8 +19,32 @@ Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile
 
 function App() {
   const [politicians, setPoliticians] = useState([]);
-
-  return <></>;
+  useEffect(() => {
+    fetch(" https://freetestapi.com/api/v1/politicians")
+      .then((response) => response.json())
+      .then((data) => setPoliticians(data))
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(politicians);
+  return (
+    <>
+      <h1>Lista politici</h1>
+      <div className="container">
+        {politicians.map((politician) => (
+          <div className="card mb-3" key={politician.id}>
+            <img
+              className="card-img-top"
+              src={politician.image}
+              alt={politician.name}
+            />
+            <h2 className="ps-2">{politician.name}</h2>
+            <h3 className="ps-2">{politician.position}</h3>
+            <p className="ps-2">{politician.biography}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
